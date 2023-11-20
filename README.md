4da4ac5c6beba3659a329828d9ec85cb96aaf33c
VollmondHeuler <looperz@mailbox.org>
2023-11-20 22:23:38 +0100

    add example batch script for copying files and executing python script

--- README.md
+++ README.md
@@ -8,5 +8,5 @@ This tool converts your yml-Files from the Minecraft plugin [Villager Market](ht
 
 - Python3 (see [requirements.txt](requirements.txt) for neccesary plugins )
 - a scheduled job providing the yml-files in the subdirectory "data"
-- a scheduled job that runs the python script data-yml2json.py to generate the json data. You can use the same job to run those two tasks one after another.
+- a scheduled job that runs the python script data-yml2json.py to generate the json data. You can use the same job to run those two tasks one after another. See [example-batch-script](mc-dealer-copy-execute.bat)
 - a webserver, that provides all the files. You may want to protect the data-Subdirectory. Feel free to use htacccess or similiar.
---
+++ mc-dealer-copy-execute.bat
@@ -0,0 +1,30 @@
+:: Batch script to copy/update VillagerMarket Shop files and execute data-yml2json.py
+::Note that this batch script only works if all data has been entered correctly
+
+@ECHO OFF
+TITLE WindowsVMCopieScript
+
+:: If the batch file is in the same folder as "data-yml2json.py", do not change anything in the following line
+:: If the batch file is NOT in the same folder as "data-yml2json.py", then change the following line to "cd path\wherethescriptis\located"
+cd /d %~dp0
+
+:: Copy and execution script starts here
+:a
+
+	:: Copies the Shop UUID.yaml files from Villagermarket to the Data folder
+	:: Change path1 to the path of your Minecraft plugin folder
+	:: Change path2 to your Shop Info webfiles folder
+	xcopy /s/y/i "path1\VillagerMarket\Shops" "path2\data" >nul
+	Echo Data has been updated
+
+	:: The python script that is being executed (Don't change this!)
+	python data-yml2json.py
+	Echo data-yml2json.py has been executed
+
+	:: Time in seconds how often the files should be updated
+	:: Villagermarket only updates its data every 10 minutes by default
+	:: A similar update time should be selected here as in the Villagermarket configuration
+	TIMEOUT /T 600 >nul
+
+::Repeat the script
+GOTO a
