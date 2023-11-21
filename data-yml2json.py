27c23cfe4aca87e39f297d4b7a1cca064895eb72
VollmondHeuler <looperz@mailbox.org>
2023-11-21 21:30:34 +0100

    introducing exchange_item (default is money, but could be an item, too)

--- data-yml2json.py
+++ data-yml2json.py
@@ -112,13 +112,20 @@ if __name__ == '__main__':
                         player_offer['item'] = item_type
                         player_offer['item'] = item_type.replace("minecraft:", "", 1)
                         player_offer['amount'] = offer_data['amount']
-                        player_offer['price'] = offer_data['price']
+                        player_offer['exchange_item'] = 'money'
+
+                        if isinstance(offer_data['price'], (int, float)):
+                            player_offer['price'] = offer_data['price']
+                        elif 'type' in offer_data['price']:
+                            player_offer['exchange_item'] = offer_data['price']['type'].replace("minecraft:", "", 1)
+                            player_offer['price'] = offer_data['price']['amount']
+                        
 
                         player_offer['price_discount'] = 0
                         if 'discount' in offer_data and 'amount' in offer_data['discount']:
                             player_offer['price_discount'] = offer_data['discount']['amount']
 
-                        player_offer['unit_price'] = round(offer_data['price'] / offer_data['amount'], 2)
+                        player_offer['unit_price'] = round(player_offer['price'] / offer_data['amount'], 2)
                         player_offer['stock'] = 0
                         player_offer['is_best_price'] = None
 
@@ -132,8 +139,15 @@ if __name__ == '__main__':
                         player_demand['item'] = item_type.replace("minecraft:", "", 1)
                         player_demand['own_name'] = None
                         player_demand['amount'] = offer_data['amount']
-                        player_demand['price'] = offer_data['buy_price']
-                        player_demand['unit_price'] = round(offer_data['price'] / offer_data['amount'], 2)
+                        player_demand['exchange_item'] = 'money'
+
+                        if isinstance(offer_data['buy_price'], (int, float)):
+                            player_demand['price'] = offer_data['buy_price']
+                        elif 'type' in offer_data['price']:
+                            player_demand['exchange_item'] = offer_data['price']['type'].replace("minecraft:", "", 1)
+                            player_demand['price'] = offer_data['price']['amount']
+
+                        player_demand['unit_price'] = round(player_demand['price']/ offer_data['amount'], 2)
                         player_demand['buy_limit'] = offer_data['buy_limit']
                         player_demand['is_best_price'] = None
 
